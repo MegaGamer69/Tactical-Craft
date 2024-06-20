ca74ee371ebb25cb5e5b7f0e0c54aa12b15361f8
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-06-20 16:01:16 -0300

    Create TacticalCraft.java

---
+++ src/main/java/org/tacticalcraft/TacticalCraft.java
@@ -0,0 +1,34 @@
+package org.tacticalcraft;
+
+// Importações Úteis
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+@Mod(TacticalCraft.modID)
+public class TacticalCraft
+{
+	// Variaveis Estáticas
+	public static final String modID = new String("Tactical Craft");
+	public static final Logger console = LogManager.getLogger();
+
+	// Construtor
+	public TacticalCraft()
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuffs);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	private void setup()
+	{
+		console.info("Não Esqueça de Seguir nossa Equipe no Twitter(o Atual X) :D");
+	}
+
+	private void doClientStuffs()
+	{
+		console.info("Não Esqueça de Seguir nossa equipe no Twitter(o Atual X) :D");
+	}
+}
