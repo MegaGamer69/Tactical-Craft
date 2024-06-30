06056878d2692bb488c9e2e3f542b468871a8b15
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-06-30 04:19:16 -0300

    Update TacticalCraft.java

--- src/main/java/org/tacticalcraft/TacticalCraft.java
+++ src/main/java/org/tacticalcraft/TacticalCraft.java
@@ -4,8 +4,8 @@ package org.tacticalcraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
