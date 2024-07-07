cb897c50420f2ae54d6f50e4da1f59146bca8ddf
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-07-07 16:28:46 -0300

    Update and rename src/main/java/org/tacticalcraft/items/bullets/Dot44Munition.java to src/main/java/org/tacticalcraft/items/TCItems.java

---
+++ src/main/java/org/tacticalcraft/items/TCItems.java
@@ -0,0 +1,22 @@
+package org.tactical.items;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.CreativeTab;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegisterEvent;
+
+import org.tacticalcraft.TacticalCraft;
+
+public class TCItems
+{
+	@ObjectHolder(TacticalCraft.MOD_ID + ":dot_44_ammo")
+	public static final Item DOT_44_ITEM = null;
+
+	@ObjectHolder(TacticalCraft.MOD_ID + ":improvised_knife")
+	public static final Item IMPROVISED_KNIFE = null;
+}
