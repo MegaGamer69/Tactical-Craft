167757d2f0faec950cea400f14917517e3e41523
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-07-06 21:10:32 -0300

    Update Dot44Munition.java

--- src/main/java/org/tacticalcraft/items/bullets/Dot44Munition.java
+++ src/main/java/org/tacticalcraft/items/bullets/Dot44Munition.java
@@ -1,6 +1,8 @@
 package org.tactical.items.bullets;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.CreativeTab;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
@@ -8,9 +10,15 @@ import net.minecraftforge.registries.ObjectHolder;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.RegisterEvent;
 
-import org.tactioncraft.TacticalCraft;
+import org.tacticalcraft.TacticalCraft;
 
-@Mod.EventBusSubscriber(modid = TacticalCraft.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@ObjectHolder(TacticalCraft.MOD_ID)
 public class Dot44Munition {
-	public static final ITEM = Registration.ITEMS.register(".44 Ammo");
+	public static final RegistryObject<Item> DOT_44_ITEM = Registration.ITEMS.register("dot_44_ammo",
+		() -> new Item(new Properties())
+	);
+
+	public static class Registration {
+		
+	}
 }
