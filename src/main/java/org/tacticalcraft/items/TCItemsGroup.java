66d03592cabd96757ad38cacd1ab2b188f32b4be
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2024-07-07 16:21:57 -0300

    Create TCItemsGroup.java

---
+++ src/main/java/org/tacticalcraft/items/TCItemsGroup.java
@@ -0,0 +1,26 @@
+package org.tacticalcraft.items;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+public class TCItemsGroup
+{
+	public static final ItemGroup HEAVY_PISTOLS = new ItemGroup("tactical_craft_heavy_pistols")
+	{
+		@Override
+		public ItemStack createIcon()
+		{
+			return new ItemStack(Items.IRON_INGOT);
+		}
+	};
+	
+	public static final ItemGroup LIGHT_KNIFES = new ItemGroup("tactical_craft_light_knifes")
+	{
+		@Override
+		public ItemStack createIcon()
+		{
+			return new ItemStack(Items.IRON_SWORD);
+		}
+	}
+}
