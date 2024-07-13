6cc294d06d86e0d85498b2d0a27be6ee24aee8e0
MegaGamerOtario <megagamerotario@gmail.com>
2024-07-13 16:23:32 -0300

    update TCItems.java

--- src/main/java/org/tacticalcraft/items/TCItems.java
+++ src/main/java/org/tacticalcraft/items/TCItems.java
@@ -1,8 +1,8 @@
 package org.tacticalcraft.items;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Properties;
+import net.minecraft.item.CreativeModeTab;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,19 +18,9 @@ public class TCItems
 {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TacticalCraft.MOD_ID);
 	
-	@ObjectHolder(TacticalCraft.MOD_ID + ":dot_44_ammo")
-	public static final Item DOT_44_AMMO = null;
+	public static final RegistryObject<Item> DOT_44_AMMO_ITEM = ITEMS.register("dot_44_ammo", () -> new Item(new Item().Properties().group(ItemGroup.ITEMS)));
 
-	@ObjectHolder(TacticalCraft.MOD_ID + ":improvised_knife")
-	public static final Item IMPROVISED_KNIFE = null;
-
-	public static final RegistryObject<Item> DOT_44_AMMO_ITEM = ITEMS.register("dot_44_ammo",
-		() -> new Item(new Item().Properties().group(ItemGroup.ITEMS))
-	);
-
-	public static final RegistryObject<Item> IMPROVISED_KNIFE_ITEM = ITEMS.register("improvised_knife",
-		() -> new Item(new Item().Properties().group(ItemGroup.COMBAT))
-	);
+	public static final RegistryObject<Item> IMPROVISED_KNIFE_ITEM = ITEMS.register("improvised_knife", () -> new Item(new Item().Properties().group(ItemGroup.COMBAT)));
 
 	@SubscribeEvent
 	public static void onRegisterItems(RegistryEvent.Register<Item> event)
